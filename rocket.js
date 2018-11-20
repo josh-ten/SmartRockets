f52221c676e7bbabb5900103f8e06f1f3e0af75b
rmit-s3657249-joshua-tencic <s3657249@student.rmit.edu.au>
2018-11-20 22:42:31 +1100

    I mean it works lol

--- rocket.js
+++ rocket.js
@@ -1,13 +1,110 @@
 class Rocket {
-  constructor() {
+  constructor(x, y, steps) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
 
+    this.steps = steps;
+    this.birthTime = frameCounter;
+    this.finishAge;
+    this.T1 = false;
+    this.T2 = false;
+
+    this.dead = false;
+    this.won = false;
   }
 
   draw() {
-
+    let dir = this.vel.heading();
+    translate(this.pos.x, this.pos.y);
+    rotate(dir);
+    rectMode(CENTER);
+    noStroke();
+    imageMode(CENTER);
+    image(rocketSprite, 0, 0, 60, 50);
+    fill(255, 200, 100);
+    if (this.T1) rect(-40, -10, 20, 10);
+    if (this.T2) rect(-40, 10, 20, 10);
+    rotate(-dir);
+    translate(-this.pos.x, -this.pos.y);
   }
 
   update() {
-    
+    if (!this.dead && !this.won) {
+      //WIN
+      if (this.distFromTarget() < 30) this.win();
+
+      //LOSE
+      if (outOfBounds(this.pos.x, this.pos.y)) this.die();
+      // if (this.pos.x < width/3 && this.pos.y > height/2) this.die();
+
+      let stepIndex = this.getAge();
+      let step = this.steps[stepIndex];
+      this.T1 = step.T1;
+      this.T2 = step.T2;
+      this.thrust(step.T1, step.T2);
+      this.kinematics();
+    }
+  }
+  
+  kinematics() {
+    this.acc.y += GRAVITY;
+    this.vel.limit(10);
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0);
+  }
+
+  thrust(T1, T2) {
+    let mag = T1 + T2;
+    let force;
+    if (this.vel.mag() > 0) 
+      force = this.vel.copy().normalize().mult(mag * ROCKET_SPEED);
+    else 
+      force = createVector(0, -ROCKET_SPEED);
+    this.acc.add(force);
+    let rotation = T1 - T2;
+    rotation *= 0.3;
+    this.vel.rotate(rotation);
+  }
+
+  getAge() {
+    return frameCounter - this.birthTime;
+  }
+
+  fitness() {
+    //Dead = 0
+    if (this.dead) 
+      return 0;
+
+    //Won = 0.1 -> 1
+    //0 < winAge < FRAMES_PER_SIM
+    if (this.won)
+      return (this.winAge / FRAMES_PER_SIM) * 0.9 + 0.1;
+
+    //Close = 0 -> 0.1
+    //distFromTarget will be > 30 as the rocket has not won
+    return 3 / this.distFromTarget();
+  }
+
+  mutate() {
+    this.steps.forEach(step => {
+      if (random() < MUTATION_CHANCE)
+        randomThrusters(step);
+    });
+  }
+
+  die() {
+    this.dead = true;
+    this.finishAge = this.getAge();
+  }
+
+  win() {
+    this.won = true;
+    this.finishAge = this.getAge();
+  }
+
+  distFromTarget() {
+    return this.pos.copy().sub(target).mag();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,150 @@
+const ROCKET_SPEED = 0.1;
+const GRAVITY = 0;
+const ROCKET_COUNT = 100;
+const FRAMES_PER_SIM = 150;
+let SIM_SPEED = 1;
+const MAX_MUTATION_CHANCE = 0.1;
+const MIN_MUTATION_CHANCE = 0.0005;
+let MUTATION_CHANCE = MAX_MUTATION_CHANCE;
+
+let generation = 0;
+let generationAge = 0;
+let frameCounter = 0;
+let rockets = [];
+let target;
+let spawnPt;
+let rocketSprite;
+let bestOfGeneration;
+let bestFitness;
+let onlyTopRocket = false;
+let simComplete = false;
+
 function setup() {
   createCanvas(window.innerWidth-3, window.innerHeight-3);
+
+  spawnPt = createVector(width/2, height);
+  target = createVector(width/2, 30);
+  rocketSprite = loadImage('./nightraiderfixed.png');
+  
+  for (let i = 0; i < ROCKET_COUNT; i++) {
+    rockets.push(new Rocket(spawnPt.x, spawnPt.y, initSteps()));
+  }
 }
 
 function draw() {
-  background(51);
-  
+  for (let i = 0; i < SIM_SPEED; i++) {
+    background(255);
+    fill(0);
+    textSize(50);
+    text('Generation ' + generation, 10, 50);
+    if (bestFitness) {
+      textSize(30);
+      text('Best fitness ' + bestFitness, 10, 90);
+    }
+    
+    generationAge++;
+    if (generationAge < FRAMES_PER_SIM) {
+      rockets.forEach(r => r.update());
+      if (!onlyTopRocket) 
+        rockets.forEach(r => r.draw());
+    } else {
+      nextGeneration();
+    }
+    if (bestOfGeneration) {
+      if (onlyTopRocket) bestOfGeneration.draw();
+      else {
+        fill(0, 255, 100);
+        ellipse(bestOfGeneration.pos.x, bestOfGeneration.pos.y, 10, 10);
+      }
+    }
+
+    drawTarget();
+
+    frameCounter++;
+  }
+}
+
+function nextGeneration(seedRocket) {
+  let topRocket;
+  let newGeneration = [];
+  if (seedRocket) {
+    topRocket = seedRocket;
+  } else {
+    let bestFitness = 0;
+    rockets.forEach(rocket => {
+      if (rocket.fitness() > bestFitness) {
+        bestFitness = rocket.fitness();
+        topRocket = rocket;
+      }
+    });
+  }
+  //Reduce mutation chance so the behaviour spread decreases as the rocket is refined
+  if (topRocket) {
+    if (MUTATION_CHANCE > MIN_MUTATION_CHANCE) MUTATION_CHANCE -= topRocket.fitness() / 10;
+    else MUTATION_CHANCE = MIN_MUTATION_CHANCE;
+  } else MUTATION_CHANCE = MAX_MUTATION_CHANCE;
+  for (let i = 0; i < ROCKET_COUNT; i++) {
+    let newBoi;
+    if (topRocket)
+      newBoi = new Rocket(spawnPt.x, spawnPt.y, copyArray(topRocket.steps));
+    else 
+      //They all died, start fresh
+      newBoi = new Rocket(spawnPt.x, spawnPt.y, initSteps());
+    if (i > 0) newBoi.mutate();
+    newGeneration.push(newBoi);
+  }
+  rockets = newGeneration;
+  bestOfGeneration = rockets[0];
+  // bestFitness = Math.round(bestOfGeneration.fitness() * 1000) / 1000;
+  bestFitness = bestOfGeneration.fitness();
+  generation++;
+  generationAge = 0;
+  simComplete = false;
+}
+
+function initSteps() {
+  let steps = [];
+  for (let i = 0; i < FRAMES_PER_SIM; i++) {
+    let step = {};
+    randomThrusters(step);
+    steps.push(step);
+  }
+    // steps.push(p5.Vector.random2D().mult(ROCKET_SPEED));
+  return steps;
+}
+
+function randomThrusters(step) {
+  step.T1 = randomBool();
+  step.T2 = randomBool();
+}
+
+function copyArray(a) {
+  let newArray = [];
+  a.forEach(el => newArray.push({'T1': el.T1, 'T2': el.T2}));
+  return newArray;
+}
+
+function randomBool() {
+  return random() < 0.5 ? true : false;
+}
+
+function mousePressed() {
+  target = createVector(mouseX, mouseY);
+}
+
+function outOfBounds(x, y) {
+  return (x > width || x < 0 || y > height || y < 0);
+}
+
+function toggleSolo() {
+  onlyTopRocket = !onlyTopRocket;
+}
+
+function drawTarget() {
+  let targetSize = 30;
+  fill(0, 255, 100);
+  stroke(0);
+  ellipse(target.x, target.y, targetSize, targetSize);
+  line(target.x, target.y-targetSize*0.6, target.x, target.y+targetSize*0.6);
+  line(target.x-targetSize*0.6, target.y, target.x+targetSize*0.6, target.y);
 }
